1c2e46c7632c3a14b4d3515953cdc27ab4d3447a
Daniel Shliakhetko <danielshliakhetko@gmail.com>
2024-10-16 13:09:43 +0300

    Rmove id types in Repository class

--- otaku_back/otaku_back/database/repository.py
+++ otaku_back/otaku_back/database/repository.py
@@ -8,17 +8,17 @@ class Repository[T: models.Model]:
     def get_all(self) -> List[T]:
         return self._model.objects.all()
 
-    def get_by_id(self, id: int) -> Optional[T]:
+    def get_by_id(self, instance_id) -> Optional[T]:
         try:
-            return self._model.objects.get(id=id)
+            return self._model.objects.get(id=instance_id)
         except self._model.DoesNotExist:
             return None
 
     def create(self, **kwargs) -> T:
         return self._model.objects.create(**kwargs)
 
-    def update(self, id: int, **kwargs) -> Optional[T]:
-        instance = self.get_by_id(id)
+    def update(self, instance_id, **kwargs) -> Optional[T]:
+        instance = self.get_by_id(instance_id)
         if instance:
             for key, value in kwargs.items():
                 setattr(instance, key, value)
@@ -26,8 +26,8 @@ class Repository[T: models.Model]:
             return instance
         return None
 
-    def delete(self, id: int) -> bool:
-        instance = self.get_by_id(id)
+    def delete(self, instance_id) -> bool:
+        instance = self.get_by_id(instance_id)
         if instance:
             instance.delete()
             return True
