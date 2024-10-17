b2f0ea802a13879cd1fde14347fbaa237563a08a
Alex Ivanov <oleksii120@gmail.com>
2024-10-17 13:43:36 +0300

    Add model serializers

--- otaku_back/otaku_back/database/schemas/genre.py
+++ otaku_back/otaku_back/database/schemas/genre.py
@@ -5,3 +5,6 @@ class Genre(models.Model):
     mal_id = models.PositiveBigIntegerField(primary_key=True)
     name = models.TextField()
     type = models.TextField()
+
+    def __str__(self):
+        return f'{self.name} ({self.type})'
