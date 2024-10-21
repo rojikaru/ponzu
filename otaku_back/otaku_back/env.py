215767dc08f1be5018e86d8db5ad3ee4312b7fe2
Alex Ivanov <oleksii120@gmail.com>
2024-10-21 22:00:33 +0300

    Fix many-to-many create & update

--- otaku_back/otaku_back/env.py
+++ otaku_back/otaku_back/env.py
@@ -2,13 +2,12 @@ import environ
 import os
 from pathlib import Path
 
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Bootstrap environment variables
 env = environ.Env(
-    DEBUG=(bool, False) # set default values and a typecast
+    DEBUG=(bool, False)  # set default values and a typecast
 )
 environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
