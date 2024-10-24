cf8e6d8a21eaf85134e9dc30629db6ab262ff995
daniel-shliakhetko <danielshliakhetko@gmail.com>
2024-10-24 15:52:36 +0300

    Setup auth, add login

---
+++ otaku_back/otaku_back/security/permissions.py
@@ -0,0 +1,11 @@
+﻿from rest_framework.permissions import BasePermission, SAFE_METHODS
+
+
+class CreateUserPermission(BasePermission):
+    def has_permission(self, request, view):
+        if view.basename == 'auth':
+            return True
+        if request.method in SAFE_METHODS and view.basename != 'auth':
+            return True
+
+        return request.user and request.user.is_authenticated
