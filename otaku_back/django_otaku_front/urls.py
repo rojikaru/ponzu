5e4086bad578a9ad4d5472ce2b4dd04711e565b2
Alex Ivanov <oleksii120@gmail.com>
2024-11-09 00:21:42 +0200

    Register views at frontend
    Co-authored-by: Danylo Shliakhetko <danielshliakhetko@gmail.com>

--- otaku_back/django_otaku_front/urls.py
+++ otaku_back/django_otaku_front/urls.py
@@ -1,12 +1,20 @@
 from django.urls import path
 
-from .views.home import HomeView
+from .views.anime import AnimeListView, AnimeCreateView, AnimeEditView, AnimeView, anime_delete
 from .views.login import LoginView
-from .views.register import RegisterView
 from .views.logout import logout_view
+from .views.register import RegisterView
 
 urlpatterns = [
-    path('', HomeView.as_view(), name='home'),
+    # path('', HomeView.as_view(), name='home'),
+
+    path('', AnimeListView.as_view(), name='home'),
+    path('anime/', AnimeListView.as_view(), name='anime_list'),
+    path('anime/create/', AnimeCreateView.as_view(), name='anime_create'),
+    path('anime/<int:pk>/', AnimeView.as_view(), name='anime_detail'),
+    path('anime/<int:pk>/delete/', anime_delete, name='anime_delete'),
+    path('anime/<int:pk>/edit/', AnimeEditView.as_view(), name='anime_edit'),
+
     path('login/', LoginView.as_view(), name='login'),
     path('register/', RegisterView.as_view(), name='register'),
     path('logout/', logout_view, name='logout'),
