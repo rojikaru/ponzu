d5f8354f445e03aea59ac9543f809cdeb0c069a2
Alex Ivanov <oleksii120@gmail.com>
2024-11-08 21:33:03 +0200

    Patch login/logout logic

--- otaku_back/django_otaku_front/views/logout.py
+++ otaku_back/django_otaku_front/views/logout.py
@@ -1,9 +1,13 @@
-﻿from django.http import JsonResponse
+﻿from django.views.generic import RedirectView
+
 from django_otaku_front.network.session import delete_tokens
 
 def logout_view(request):
-    user_session = request.session.get('user_session')
-    if user_session:
-        delete_tokens(user_session)
+    session_id = request.session.get('session_id')
+    if session_id:
+        delete_tokens(session_id)
     request.session.flush()
-    return JsonResponse({'message': 'Logged out successfully'})
+    request.user = None
+
+    # Redirect to main page
+    return RedirectView.as_view(url='/')(request)
--- otaku_back/django_otaku_front/views/register.py
+++ otaku_back/django_otaku_front/views/register.py
@@ -1,8 +1,8 @@
 ﻿from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, RedirectView
 from django_otaku_front.forms.register import RegisterForm
-from django_otaku_front.network.session import create_session, get_session, set_access_token, delete_tokens
-from django_otaku_front.network.helper import get_full_url
+from django_otaku_front.network.helper import get_full_url, register
+
 
 class RegisterView(TemplateView):
     template_name = 'register.html'
@@ -14,23 +14,22 @@ class RegisterView(TemplateView):
 
     def post(self, request, *args, **kwargs):
         form = RegisterForm(request.POST)
-        if form.is_valid():
-            username = form.cleaned_data['username']
-            password = form.cleaned_data['password']
-            email = form.cleaned_data['email']
-            session_id = create_session()
-            session = get_session(session_id)
-            response = session.post(get_full_url('auth/register/'), data={'username': username, 'password': password, 'email': email})
-            if response.status_code == 200 or response.status_code == 201:
-                tokens = response.json()
-                access_token = tokens.get('access_token')
-                refresh_token = tokens.get('refresh_token')
-                print('access_token:', access_token)
-                if refresh_token:
-                    request.session['access_token'] = access_token
-                    request.session['refresh_token'] = refresh_token
-                    request.session['session_id'] = session_id
-                    set_access_token(session, access_token)
-                else:
-                    delete_tokens(session)
-        return render(request, self.template_name, {'form': form, 'title': 'Register', 'description': 'Please enter your details to register.'})
+        if not form.is_valid():
+            return render(
+                request, 
+                self.template_name, 
+                {'form': form, 'title': 'Register', 'description': 'Invalid credentials, try again!'}
+            )
+        
+        username = form.cleaned_data['username']
+        password = form.cleaned_data['password']
+        email = form.cleaned_data['email']
+        
+        if register(request, username, email, password):
+            return RedirectView.as_view(url='/')(request)
+        else:
+            return render(
+                request,
+                self.template_name,
+                {'form': form, 'title': 'Register', 'description': 'Please enter your details to register.'}
+            )
