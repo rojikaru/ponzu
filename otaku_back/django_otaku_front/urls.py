5754f262ced6fee165b34d1f15d80caf0518dea0
daniel-shliakhetko <danielshliakhetko@gmail.com>
2024-11-18 16:42:51 +0200

    Add graph and dashboard display

--- otaku_back/django_otaku_front/urls.py
+++ otaku_back/django_otaku_front/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 
 from .views.anime import AnimeListView, AnimeCreateView, AnimeEditView, AnimeView, anime_delete
+from .views.graph import dashboard_redirect, DashboardViewSet, GraphViewSet
 from .views.login import LoginView
 from .views.logout import logout_view
 from .views.register import RegisterView
@@ -14,7 +15,9 @@ urlpatterns = [
     path('anime/<int:pk>/', AnimeView.as_view(), name='anime_detail'),
     path('anime/<int:pk>/delete/', anime_delete, name='anime_delete'),
     path('anime/<int:pk>/edit/', AnimeEditView.as_view(), name='anime_edit'),
-
+    path('dashboard/', dashboard_redirect, name='dashboard'),
+    path('dashboard/<str:version>/', DashboardViewSet.as_view(), name='dashboard_page'),
+    path('dashboard/<str:version>/<str:graph>/', GraphViewSet.as_view(), name='dashboard_graph'),
     path('login/', LoginView.as_view(), name='login'),
     path('register/', RegisterView.as_view(), name='register'),
     path('logout/', logout_view, name='logout'),
