17eabc49cc947a3571db77d1e64d4feea82157d9
Alex Ivanov <oleksii120@gmail.com>
2024-11-21 09:45:16 +0200

    Convert repository to async

--- otaku_back/otaku_back/database/repository.py
+++ otaku_back/otaku_back/database/repository.py
@@ -1,26 +1,27 @@
-from typing import List, Optional
+from typing import List, Optional, Type, Dict, Any
 
 from django.core.exceptions import BadRequest
 from django.db import models
+from asgiref.sync import sync_to_async
 
 
 class Repository[T: models.Model]:
-    def __init__(self, model: type[T]):
+    def __init__(self, model: Type[T]):
         self._model = model
 
-    def get_all(self) -> List[T]:
-        return list(self._model.objects.all())
+    async def get_all(self) -> List[T]:
+        return await sync_to_async(self._model.objects.all)()
 
-    def get_by_id(self, instance_id) -> Optional[T]:
+    async def get_by_id(self, instance_id) -> Optional[T]:
         try:
             primary_key_arg = {self._get_primary_key(): instance_id}
-            return self._model.objects.get(**primary_key_arg)
+            return await self._model.objects.aget(**primary_key_arg)
         except self._model.DoesNotExist:
             return None
 
-    def create(self, **kwargs) -> T:
+    async def create(self, **kwargs) -> T:
         # Check if the model exists
-        if self.exists(kwargs.get(self._get_primary_key())):
+        if await self.exists(kwargs.get(self._get_primary_key())):
             raise BadRequest(f'{self._model.__name__} already exists')
 
         related_fields = self._get_related_fields()
@@ -28,14 +29,14 @@ class Repository[T: models.Model]:
         # Separate related fields from other fields
         related_data = {k: kwargs.pop(k) for k in list(kwargs.keys()) if k in related_fields}
 
-        instance = self._model.objects.create(**kwargs)
-        return self._handle_related_fields(instance, related_data)
+        instance = await self._model.objects.acreate(**kwargs)
+        return await self._handle_related_fields(instance, related_data)
 
-    def update(self, instance_id, **kwargs) -> Optional[T]:
+    async def update(self, instance_id, **kwargs) -> Optional[T]:
         if kwargs.get(self._get_primary_key()) is not None:
             raise BadRequest('Changing id is prohibited')
 
-        instance = self.get_by_id(instance_id)
+        instance = await self.get_by_id(instance_id)
         if not instance:
             return None
 
@@ -48,25 +49,28 @@ class Repository[T: models.Model]:
         for key, value in kwargs.items():
             setattr(instance, key, value)
 
-        instance.save()
+        await instance.asave()
 
         # Handle many-to-many updates
-        self._handle_related_fields(instance, related_data)
+        await self._handle_related_fields(instance, related_data)
 
-        instance.save()
+        await instance.asave()
         return instance
 
-    def delete(self, instance_id) -> bool:
-        instance = self.get_by_id(instance_id)
+    async def delete(self, instance_id) -> bool:
+        instance = await self.get_by_id(instance_id)
         if not instance:
             return False
 
-        instance.delete()
+        await instance.adelete()
         return True
 
-    def exists(self, instance_id) -> bool:
+    async def exists(self, instance_id) -> bool:
         primary_key_arg = {self._get_primary_key(): instance_id}
-        return self._model.objects.filter(**primary_key_arg).exists()
+        return await self._model.objects.filter(**primary_key_arg).aexists()
+
+    async def aggregate(self, **kwargs) -> Dict[str, Any]:
+        return await self._model.objects.aaggregate(**kwargs)
 
     def _get_related_fields(self) -> List[str]:
         """
@@ -84,21 +88,21 @@ class Repository[T: models.Model]:
         """
         return self._model._meta.pk.name
 
-    def _handle_related_fields(self, instance, related_data):
+    async def _handle_related_fields(self, instance, related_data):
         # Handle foreign fields
         for field, related_ids in related_data.items():
             # if the field is a many-to-many field
             field_attr = getattr(self._model, field).field
             if isinstance(field_attr, models.ManyToManyField):
-                getattr(instance, field).set(related_ids)
+                await getattr(instance, field).asave(related_ids)
 
             # if the field is a foreign key field
             else:
                 # Fetch related field
                 related_model = self._model._meta.get_field(field).related_model
                 related_pk = related_model._meta.pk.name
-                related_obj = related_model.objects.get(**{related_pk: related_ids})
+                related_obj = await related_model.objects.aget(**{related_pk: related_ids})
                 setattr(instance, field, related_obj)
 
-        instance.save()
+        await instance.asave()
         return instance
