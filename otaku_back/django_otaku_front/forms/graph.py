8d89c3252f0e5556cd36f9a6a5827a3374bc0483
Alex Ivanov <oleksii120@gmail.com>
2025-01-21 19:12:59 +0200

    Remove django project

--- otaku_back/django_otaku_front/forms/graph.py
+++
@@ -1,6 +0,0 @@
-from django import forms
-
-
-class GraphForm(forms.Form):
-    min_year = forms.IntegerField(required=False)
-    max_year = forms.IntegerField(required=False)
--- otaku_back/django_otaku_front/forms/login.py
+++
@@ -1,5 +0,0 @@
-﻿from django import forms
-
-class LoginForm(forms.Form):
-    username = forms.CharField(max_length=100, required=True)
-    password = forms.CharField(widget=forms.PasswordInput, required=True)
