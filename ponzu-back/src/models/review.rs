0d23e920fb033003f159985f10d973e40cf6c250
Alex Ivanov <oleksii120@gmail.com>
2025-01-25 16:36:08 +0200

    Add review models

---
+++ ponzu-back/src/models/review.rs
@@ -0,0 +1,25 @@
+use serde::{Deserialize, Serialize};
+use crate::models::title::{Anime, Manga};
+
+#[derive(PartialEq, Debug, Serialize, Deserialize)]
+pub struct Review {
+    pub user: i32,
+    pub score: i32,
+    pub content: String,
+    pub created_at: Option<chrono::DateTime<chrono::Utc>>,
+    pub updated_at: Option<chrono::DateTime<chrono::Utc>>,
+}
+
+#[derive(PartialEq, Debug, Serialize, Deserialize)]
+pub struct AnimeReview {
+    #[serde(flatten)]
+    review: Review,
+    anime: Anime,
+}
+
+#[derive(PartialEq, Debug, Serialize, Deserialize)]
+pub struct MangaReview {
+    #[serde(flatten)]
+    review: Review,
+    manga: Manga,
+}
